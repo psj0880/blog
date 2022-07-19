8c2685b0ce84370613f5908423d6c532339aa5dc
George <george.park@bunjang.co.kr>
2022-07-19 16:47:34 +0900

    fix: published: true만 가져오게 변경

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -7,20 +7,21 @@ import { Post, FrontMatter, Tag } from '../interfaces'
 export function getAllPosts(): Post[] {
   const paths = sync(`${process.cwd()}/posts/**/*.md`)
 
-  const posts = paths.map<Post>((path) => {
+  const posts = paths.reduce((result: Post[], path) => {
     const md = fs.readFileSync(path, { encoding: 'utf-8' })
     const { attributes, body } = frontMatter<FrontMatter>(md)
 
-    const post: Post = {
-      frontMatter: {
-        ...attributes,
-        date: moment(attributes.date).format('yyy-MM-DD'),
-      },
-      body: body,
-    }
+    if (attributes.published)
+      result.push({
+        frontMatter: {
+          ...attributes,
+          date: moment(attributes.date).format('yyy-MM-DD'),
+        },
+        body: body,
+      })
 
-    return post
-  })
+    return result
+  }, [])
 
   posts.sort((p1, p2) => {
     if (p1.frontMatter.date < p2.frontMatter.date) return 1
